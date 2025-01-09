10b5fd63d8b9f96ca69dfc4c2cad07211f5b2b73
Lily <egzozu.be.bas@gmail.com>
2025-01-09 18:05:08 +0300

    retire showAlertDialog

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -182,10 +182,10 @@ class CoolUtil
 
 	public static function showPopUp(message:String, title:String):Void
 	{
-		#if android
+		/*#if android
 		AndroidTools.showAlertDialog(title, message, {name: "OK", func: null}, null);
-		#else
+		#else*/
 		FlxG.stage.window.alert(message, title);
-		#end
+		//#end
 	}
 }
--- source/openfl/display/Shader.hx
+++ source/openfl/display/Shader.hx
@@ -346,9 +346,10 @@ class Shader
 			}
 			#end
 			if (compileStatus == 0)
-				#if (android && !macro)
+				/*#if (android && !macro)
 				android.Tools.showAlertDialog("Shader Compile Error!", message, {name: "OK", func: null}, null)
-				#elseif !ios
+				#elseif !ios*/
+				#if !macro
 				openfl.Lib.application.window.alert('$message', 'Shader Compile Error!')
 				#else
 				Log.error(message)
